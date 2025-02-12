a6fe2e30419b7768c4a2beccd6f0fd07e89ce350
Gulshan Ahmad <gulshanahmad68@gmail.com>
2025-02-12 22:00:19 +0500

    Change the inline css to external css in navbar.css

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+
+import "./Navbar.css";
+const Navbar=()=>{
+    return(
+        <nav className="navbar">
+            <h1> 🍔 My Restaurent App</h1> 
+            <ul >
+                <li>Home</li>
+                <li>Menu</li>
+                <li>About-us</li>
+            </ul>
+        </nav>
+            
+        
+    )
+}
+export default Navbar
