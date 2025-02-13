e24acd83de2ee4cefec938b16c4b51b11e9448e9
Gulshan Ahmad <gulshanahmad68@gmail.com>
2025-02-13 12:37:45 +0500

    Add navbar for checking

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 
 import Fooditem from "./components/FoodItems/FoodItem";
+import Navbar from "./components/Navbar";
 function App (){
   return(
     <div>
-
+      <Navbar/>
       <Fooditem/>
     </div>
   )
