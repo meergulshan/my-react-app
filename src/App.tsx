b836e5d84a7d386815094fab4385e88d6665de44
Gulshan Ahmad <gulshanahmad68@gmail.com>
2025-02-13 12:33:48 +0500

    add only fooditem component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 
 import Fooditem from "./components/FoodItems/FoodItem";
-import Navbar from "./components/Navbar";
 function App (){
   return(
     <div>
-      <Navbar/>
+
       <Fooditem/>
     </div>
   )
