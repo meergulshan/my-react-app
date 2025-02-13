707b8ca9c6c6afa90c93fd228905d696477629b9
Gulshan Ahmad <gulshanahmad68@gmail.com>
2025-02-13 21:41:58 +0500

    Change the code into into a array nd jason file ...

--- src/components/FoodItems/FoodItem.tsx
+++ src/components/FoodItems/FoodItem.tsx
@@ -1,53 +1,25 @@
-import "./FoodItem.css"
-const Fooditem=()=>{
+import "./FoodItem.css";
+import { FoodItems } from "./Fooditem.utils";
 
-    return(
-     <div>
-         <h1  className="food-heading"> 🍔 Our Delicious Food Items 🍗</h1>
-             <div className="food-grid">
-                  <div className="food-card">
-                   <img src="/MyApp-Assets/pngwing.com.png" />
-                   <div className="food-title">Delicious Zinger Burger ➡️ $13</div>
-             </div>
+const Fooditem = () => {
+  return (
+    <div>
+      <h1 className="food-heading"> 🍔 Our Delicious Food Items 🍗</h1>
+      <div className="food-grid">
+        {FoodItems.map((item, index) => {
+          return (
+            <div className="food-card" key={`${index}-${item.name}`}>
+              <img src={item.image} />
+              <div className="food-title">
+                {item.name} ➡️ {item.price}
+              </div>
+            </div>
+          );
+        })}
 
-              <div className="food-card">
-                  <img src="/MyApp-Assets/pngwing.com (1).png"  />
-                   <div className="food-title">Hot Wings ➡️$10</div>
-               </div>
-
-              <div className="food-card">
-                     <img src="/MyApp-Assets/pngwing.com (2).png" />
-                     <div className="food-title">Roasted Chicken Pieces ➡️$15</div>
-                </div>
-
-               <div className="food-card">
-                  <img src="/MyApp-Assets/pngwing.com (3).png" />
-                  <div className="food-title">Turkish Stuffed Eggplant ➡️$12</div>
-                </div>
-                  <div className="food-card">
-                     <img src="/MyApp-Assets/menu1.png" />
-                      <div className="food-title">Paneer Tikka ➡️$100</div>
-
-                 </div>
-                 <div className="food-card">
-                     <img src="/MyApp-Assets/menu2.png" />
-                      <div className="food-title">Delicious Platter ➡️$200</div>
-
-                 </div>
-                 <div className="food-card">
-                     <img src="/MyApp-Assets/menu3.png" />
-                      <div className="food-title">Chicken Nodels ➡️$100</div>
-
-                 </div>
-                 <div className="food-card">
-                     <img src="/MyApp-Assets/pngwing.com.png" />
-                      <div className="food-title">Delicious Zinger Burger ➡️ $13</div>
-
-                 </div>
-                
-
-          </div>
-        </div>
-        )
-}
-export default Fooditem
+       
+      </div>
+    </div>
+  );
+};
+export default Fooditem;
---
+++ src/components/FoodItems/Fooditem.utils.ts
@@ -0,0 +1,45 @@
+export const FoodItems = [
+    {
+      name: "Delicious Zinger Burger",
+      price: "$13",
+      image: "/MyApp-Assets/pngwing.com.png",
+    },
+    {
+      name: "Delicious Platter",
+      price: "$200",
+      image: "/MyApp-Assets/menu2.png",
+    },
+    {
+      name: "Paneer Tikka",
+      price: "$100",
+      image: "/MyApp-Assets/menu1.png",
+    },
+    {
+      name: "Chicken Nodels",
+      price: "$100",
+      image: "/MyApp-Assets/menu3.png",
+    },
+    {
+        name:"Turkish Stuffed Eggplant",
+        price:"$170",
+        image:"MyApp-Assets/pngwing.com (3).png"
+    },
+    {
+        name:"Roasted Chicken Pieces",
+        price:"$233",
+        image:"/MyApp-Assets/pngwing.com (2).png"
+    },
+    {
+        name:"Hot wings",
+        price:"$199",
+        image:"/MyApp-Assets/pngwing.com (1).png"
+    },
+    {
+        name: "Delicious Zinger Burger",
+        price: "$13",
+        image: "/MyApp-Assets/pngwing.com.png",
+      }
+  ];
+
+
+  export const RandomFoodItems=[]
