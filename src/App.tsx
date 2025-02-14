1c2a7c9eabc83c5e4cbf16e362651868f787e34d
Gulshan Ahmad <gulshanahmad68@gmail.com>
2025-02-14 14:37:55 +0500

    HomePage Added in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 
-import Fooditem from "./components/FoodItems/FoodItem";
-import Navbar from "./components/Navbar/Navbar";
+import HomePage from "./pages/HomePage";
 function App (){
   return(
     <div>
-      <Navbar/>
-      <Fooditem/>
+      <HomePage/>
     </div>
   )
 }
